46b4b3538d5740a90bbdcf5d3c531a3c777677a6
rharmse <rudolf.harmse@gmail.com>
2019-07-14 20:09:50 +0200

    30DoC Day2

---
+++ src/main/java/code/rharmse/thirtydoc/Day3.java
@@ -0,0 +1,33 @@
+package code.rharmse.thirtydoc;
+
+import java.util.Scanner;
+
+public class Day3 {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        System.out.printf("%d", 
+            Math.round(meal_cost + 
+                (meal_cost * (tip_percent/100.0d)) + 
+                (meal_cost * (tax_percent/100.0d))
+            )
+        );
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
